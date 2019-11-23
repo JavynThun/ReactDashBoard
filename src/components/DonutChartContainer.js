ae45ec5cb9e1e6f0f3e414b186b5b45f60a1dc89
Javyn Thun <thunjavyn@gmail.com>
2019-11-23 22:29:21 +0800

    added centre of donut chart

--- src/components/DonutChartContainer.js
+++ src/components/DonutChartContainer.js
@@ -11,13 +11,14 @@ import {
 
 import { donutChartData } from '../data/appData';
 
+const donutCenterRenderer = () => (<span><h3>Accounts</h3> </span>);
 /* This function's outputted string determines the label contents */
 const labelTemplate = (e) => (e.category + '\n'  + (e.percentage*100) +'%');
 
 export const DonutChartContainer = () => (
-  <Chart style={{height:300}}>
+  <Chart style={{height:300}} donutCenterRender={donutCenterRenderer}>
     <ChartSeries>
-      <ChartSeriesItem type="donut" data={donutChartData} categoryField="accountType" field="percent" padding={0}>
+      <ChartSeriesItem type="donut" data={donutChartData} categoryField="accountType" field="percent" padding={0} startAngle = {200}>
         <ChartSeriesLabels color="#fff" background="none" content={labelTemplate} />
       </ChartSeriesItem>
     </ChartSeries>
